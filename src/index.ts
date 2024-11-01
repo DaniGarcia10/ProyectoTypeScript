e10fc064a0944495f15a10496f8845593b25c06c
DaniGarcia10 <danielgarciapelaez@gmail.com>
2024-11-02 00:30:55 +0100

    Punto 3

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,61 @@ function handleMenuClick(event: Event): void {
         divisorFavoritos.classList.remove('d-none');
     }
 
-
 }
 
-//Añadir el evento de clic a todos los elemento del menu
+//evento de clic a todos los elemento del menu
 menuItems.forEach(item => {
     item.addEventListener('click', handleMenuClick);
 });
 
+// Referencias a los elementos HTML
+let botonBuscar = document.getElementById('btn-search') as HTMLButtonElement;
+let card = document.getElementById('card') as HTMLElement;
+let elementoTitulo = document.getElementById('card-title') as HTMLElement;
+let elemento1 = document.getElementById("li-data1") as HTMLElement | null;
+let elemento2 = document.getElementById("li-data2") as HTMLElement | null;
+
+// Funcion asincrona para buscar pais en el array de paises
+async function buscarPais(): Promise<void> {
+    // Llamada a funcion que busca paises en la API
+    let paises = await obtenerPaises();
+    mostrarPais(paises[0]);
+}
+
+// Funcion mostrar datos de pais en card
+async function mostrarPais(pais: Pais): Promise<void> {
+    //Compruebo que el pais existe
+    if (pais) {
+        card.classList.remove('d-none');
+        elementoTitulo.textContent = pais.getNombre();
+        //compruebo que los elementos existen
+        if (elemento1) {
+            elemento1.textContent = `Capital: ${pais.getCapital() || 'Desconocida'}`;
+        } else {
+            console.warn("Elemento li-data1 no encontrado");
+        }
+        if (elemento2) {
+            elemento2.textContent = `Región: ${pais.getRegion() || 'Desconocida'}`;
+        } else {
+            console.warn("Elemento li-data2 no encontrado");
+        }
+
+    }
+}
+
+// funcion obtener todos paises de la API
+async function obtenerPaises(): Promise<Pais[]> {
+    try {
+        let response = await fetch(`https://restcountries.com/v3.1/all`);
+        // Compruebo si la respuesta es correcta
+        let arrayPaises: [] = await response.json();
+        //Paso del json a objetos Pais
+        return arrayPaises.map((item: any) => new Pais(item.cca3, item.name.common, item.capital, item.region));
+    } catch (error) {
+        console.error("Error al obtener los datos de los países", error);
+        return [];
+    }
+}
+
+//Evento clic en el boton buscar
+botonBuscar.addEventListener('click', buscarPais);
